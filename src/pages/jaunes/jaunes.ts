3e27b9d28f4c1440cff8589eda4f93202c00122b
zouhair25 <zouhair.fassih@gmail.com>
2019-01-16 16:46:32 +0000

    ajout d autour de moi

--- src/pages/jaunes/jaunes.ts
+++ src/pages/jaunes/jaunes.ts
@@ -26,7 +26,7 @@ export class JaunesPage{
   searchTerm = new Subject<string>();
   searchTermOu = new Subject<string>();
   quiquoi: string='';
-  ou: string ='';
+  ou: string ='autour de moi';
   //tel: string ='0522777100';
   tel: string ='';
   searching: any =false;
@@ -36,6 +36,9 @@ export class JaunesPage{
   lng;
   //pro ou inv
   type: string;
+
+  storageVille: any =['Autour de moi'];
+  hasFocus: boolean =false;
  // @ViewChild('searchbar') searchBox: Searchbar;
   constructor(public navCtrl: NavController, public navParams: NavParams,
               private el: ElementRef,
@@ -43,6 +46,7 @@ export class JaunesPage{
               private storage: Storage) {
 
   }
+
    getLocation(){
         this.geolocation.getCurrentPosition().then((resp)=>{
      console.log('Location', resp);
@@ -51,6 +55,20 @@ export class JaunesPage{
     }).catch((error) => {
      console.log('Error getting location', error);
    });
+    }
+
+     
+    storeHistoriqueVille(){
+      //initialiser historique ville par autour de moi
+      //this.storage.set('storeVil','Autour de moi');
+    }
+    getHistoriqueVille(){
+        this.storage.get('storeVil').then((val) => {
+            this.storageVille=val;
+           console.log('Your age is', this.storageVille);
+   
+  });
+       return this.storageVille;
     }
     onGoBlanchesPage(){
       this.showBlanches = true;
@@ -63,8 +81,10 @@ export class JaunesPage{
     onGoJaunesPage(){
       this.showBlanches = false;
       this.showJaune = true;
+      
       setTimeout(() => {
         this.searchBox.setFocus();
+
       },500);
     }
     search(term: string): void {
@@ -75,10 +95,11 @@ export class JaunesPage{
       this.searchTermOu.next(term);
     }
     back(lat,lng){
-    	this.navCtrl.push('SearchPage',{lat: this.lat,lng: this.lng});
+      this.navCtrl.push('SearchPage',{lat: this.lat,lng: this.lng});
     }
  
     ionViewDidLoad() {
+
       this.getLocation();
            // recuperation de type pro ou inv pour savoir quel est à afficher
            this.type=this.navParams.get('type');
@@ -104,6 +125,7 @@ export class JaunesPage{
            switchMap((term: string)=> this.auto_ouLoadXML(term)),
             );
            
+          
     }
 
     selectValueQuiquoi(item){
@@ -115,6 +137,10 @@ export class JaunesPage{
           switchMap((term: string) => this.auto_quiquoiLoadXML(term)));
     }
     selectValueOu(item){
+
+      this.storageVille.push(item.title);
+      this.storage.set('storeVil',this.storageVille);
+      console.log('iieiei',item);
            this.ou=item.title;
            this.xmlOu$ =this.searchTermOu.pipe(
            debounceTime(1000),
@@ -122,17 +148,30 @@ export class JaunesPage{
            switchMap((term: string)=> this.auto_ouLoadXML(term)),
            );
     }
+        selectValueOuHistorique(item){
+            console.log('iieiei',item);
+            this.ou=item;
+            this.storageVille=[];
+           //this.storage.set('storeVille',item.storeVille);
+    }
 
     onSearchInput(){
         this.searching = true;
     }
-
+     checkFocusQuiquoi(){
+        //pour ne pas initialiser la liste des villes par autour de moi
+        this.hasFocus=false;
+     }
         checkFocus(){
           this.xmlItems$ = this.searchTerm.pipe(
           debounceTime(1500),
           distinctUntilChanged(),
           switchMap((term: string) => this.auto_quiquoiLoadXML(term)),
          );
+           //pour  initialiser la liste des villes par autour de moi
+          this.hasFocus=true;
+          //storage recuperer value d historique
+         console.log('fff',this.storageVille);
     }
     
     /* onSubmitForm(quioui: string, ou: string){
@@ -140,8 +179,8 @@ export class JaunesPage{
     }*/
 
         // permet de tester si quiquoi ou ville est vide sinon il redirege vers la page search-jaune
-    onDisplay(quiquoi, ou){
-    	         console.log('itemitemitemitem');
+    onDisplay(quiquoi, ou,lat,lng){
+               console.log('itemitemitemitem');
            if(this.quiquoi=='' || this.quiquoi==null){        
                setTimeout(() => {
                 this.searchBox.setFocus();
@@ -152,7 +191,7 @@ export class JaunesPage{
                 console.log('Focus',this.searchVil);
                },10);
            }else {
-           this.navCtrl.push(SearchJaunePage,{ou: this.ou, quiquoi: this.quiquoi})
+           this.navCtrl.push(SearchJaunePage,{ou: this.ou, quiquoi: this.quiquoi,lat: this.lat,lng: this.lng})
            }
     }
       onDisplayBlanches(tel){
@@ -171,173 +210,173 @@ export class JaunesPage{
 
 
     auto_quiquoiLoadXML(term: string){
-      console.log(this.auto_quiquoiParseXML(term));
+      
       return this.auto_quiquoiParseXML(term);      
     }
 
     auto_ouLoadXML(term: string){
-      console.log(this.auto_ouParseXML(term));
+   
       return this.auto_ouParseXML(term);      
     }
 
 
     auto_quiquoiParseXML(quiquoi){
          let yyy: any = [];
-	      return new Promise((resolve,  reject) =>{
+        return new Promise((resolve,  reject) =>{
 
-	        var autocomplete_quiquoi = quiquoi; 
-	        var data_send  = '<?xml version="1.0" encoding="UTF-8" ?>';
-	        data_send += '  <methodcall>';
-	        data_send += '    <methodname call="ann_sug">';  
-	        data_send += '      <params>';  
-	        data_send += '        <value>';
-	        data_send += '          <string>'+autocomplete_quiquoi+'</string>';
-	        data_send += '          <extract>5</extract>';  
-	        data_send += '        </value>';
-	        data_send += '      </params>';  
-	        data_send += '    </methodname>';
-	        data_send += '  </methodcall>';
-	  
-	    
-		        $.ajax({
-		          
-		          type       : "POST",
-		          url        : "https://www.telecontact.ma/WsMobTlC2014nVZA",
-		          /*headers: {accepts: '*'}:*/
-		          crossDomain: true,
-		          beforeSend : function() {$("#results_loading").append('<div class="noresults"><br /><br />Veuillez patienter<br /><br /><img src="media/images/home/load_result.gif" /></div>');/*$.mobile.loading('show')*/},
-		          complete   : function() {$("#results_loading").hide();/*$.mobile.loading('hide')*/},
-		          data       : {telecontact : data_send},
-		          dataType   : 'text',
-		          success    : function(response) {
+          var autocomplete_quiquoi = quiquoi; 
+          var data_send  = '<?xml version="1.0" encoding="UTF-8" ?>';
+          data_send += '  <methodcall>';
+          data_send += '    <methodname call="ann_sug">';  
+          data_send += '      <params>';  
+          data_send += '        <value>';
+          data_send += '          <string>'+autocomplete_quiquoi+'</string>';
+          data_send += '          <extract>5</extract>';  
+          data_send += '        </value>';
+          data_send += '      </params>';  
+          data_send += '    </methodname>';
+          data_send += '  </methodcall>';
+    
+      
+            $.ajax({
+              
+              type       : "POST",
+              url        : "https://www.telecontact.ma/WsMobTlC2014nVZA",
+              /*headers: {accepts: '*'}:*/
+              crossDomain: true,
+              beforeSend : function() {$("#results_loading").append('<div class="noresults"><br /><br />Veuillez patienter<br /><br /><img src="media/images/home/load_result.gif" /></div>');/*$.mobile.loading('show')*/},
+              complete   : function() {$("#results_loading").hide();/*$.mobile.loading('hide')*/},
+              data       : {telecontact : data_send},
+              dataType   : 'text',
+              success    : function(response) {
                      response =response.replace(/&(?!(?:apos|quot|[gl]t|amp);|#)/g, '&amp;');
-		            let parser = new xml2js.Parser(
-		                   {
-		                      trim: true,
-		                      explicitArray: true
-		                   });
-		               parser.parseString(response, function (err, result)
-		              {
-		                
-		                    
-		               	
+                let parser = new xml2js.Parser(
+                       {
+                          trim: true,
+                          explicitArray: true
+                       });
+                   parser.parseString(response, function (err, result)
+                  {
+                    
+                        
+                     
                        console.log('err',err);
-		               for(let answers of result.search_answers.search_answer) {
-		               	   // console.log('answers d',answers.items[0].$.count);
-		               	    
+                   for(let answers of result.search_answers.search_answer) {
+                        // console.log('answers d',answers.items[0].$.count);
+                         
 
-		                     	if (answers.items[0]=="			") {
+                           if (answers.items[0]=="      ") {
 
-		               		               // yyy.push({'title': 'Aucun resultat'}); 
-		              	        }else{
+                                      // yyy.push({'title': 'Aucun resultat'}); 
+                            }else{
 
-				                 for(let item of answers.items){
-				                                  
-				                   for(let i of item.item){
-				                     
-				                   yyy.push({'title': i.name[0]});                     
-				                     /*for(let i_data of i.item_data){
-				                        //console.log(i_data.data[1]._, i_data.data[1].$.name);
-				                        yyy.push({'title': i_data.data[1]._});
-				                       for(let data of i_data.data){
-				                        
-				                         yyy.push({key: data.$.name, value: data._});
-				                         
-				                       }
-				                     }*/
-				                   }
-				                 }
+                         for(let item of answers.items){
+                                          
+                           for(let i of item.item){
+                             
+                           yyy.push({'title': i.name[0]});                     
+                             /*for(let i_data of i.item_data){
+                                //console.log(i_data.data[1]._, i_data.data[1].$.name);
+                                yyy.push({'title': i_data.data[1]._});
+                               for(let data of i_data.data){
+                                
+                                 yyy.push({key: data.$.name, value: data._});
+                                 
+                               }
+                             }*/
+                           }
+                         }
 
-		                        }
-		               }
-		               resolve(yyy);
-		         
-		              });
-		          }, 
-		          error: function(error){
-		            console.log(error);
-		            reject();
+                            }
+                   }
+                   resolve(yyy);
+             
+                  });
+              }, 
+              error: function(error){
+                console.log(error);
+                reject();
 
-		          }
-		        });
-	      });
+              }
+            });
+        });
     }
 
     auto_ouParseXML(ou){
          let yyy: any = [];
-	        return new Promise((resolve,  reject) =>{
+          return new Promise((resolve,  reject) =>{
 
-	          var autocomplete_ou = ou; 
-	          var data_send  = '<?xml version="1.0" encoding="UTF-8" ?>';
-	            data_send += '  <methodcall>';
-	            data_send += '    <methodname call="ann_sug">';  
-	            data_send += '      <params>';  
-	            data_send += '        <value>';
-	            data_send += '          <ou>'+autocomplete_ou+'</ou>';
-	            data_send += '          <extract>5</extract>';  
-	            data_send += '        </value>';
-	            data_send += '      </params>';  
-	            data_send += '    </methodname>';
-	            data_send += '  </methodcall>';
-	    
-	      
-	          $.ajax({
-	            
-	            type       : "POST",
-	            url        : "https://www.telecontact.ma/WsMobTlC2014nVZA",
-	            /*headers: {accepts: '*'}:*/
-	            crossDomain: true,
-	            beforeSend : function() {$("#results_loading").append('<div class="noresults"><br /><br />Veuillez patienter<br /><br /><img src="media/images/home/load_result.gif" /></div>');/*$.mobile.loading('show')*/},
-	            complete   : function() {$("#results_loading").hide();/*$.mobile.loading('hide')*/},
-	            data       : {telecontact : data_send},
-	            dataType   : 'text',
-	            success    : function(response) {
+            var autocomplete_ou = ou; 
+            var data_send  = '<?xml version="1.0" encoding="UTF-8" ?>';
+              data_send += '  <methodcall>';
+              data_send += '    <methodname call="ann_sug">';  
+              data_send += '      <params>';  
+              data_send += '        <value>';
+              data_send += '          <ou>'+autocomplete_ou+'</ou>';
+              data_send += '          <extract>5</extract>';  
+              data_send += '        </value>';
+              data_send += '      </params>';  
+              data_send += '    </methodname>';
+              data_send += '  </methodcall>';
+      
+        
+            $.ajax({
+              
+              type       : "POST",
+              url        : "https://www.telecontact.ma/WsMobTlC2014nVZA",
+              /*headers: {accepts: '*'}:*/
+              crossDomain: true,
+              beforeSend : function() {$("#results_loading").append('<div class="noresults"><br /><br />Veuillez patienter<br /><br /><img src="media/images/home/load_result.gif" /></div>');/*$.mobile.loading('show')*/},
+              complete   : function() {$("#results_loading").hide();/*$.mobile.loading('hide')*/},
+              data       : {telecontact : data_send},
+              dataType   : 'text',
+              success    : function(response) {
                     
-	              let parser = new xml2js.Parser(
-	                     {
-	                        trim: true,
-	                        explicitArray: true
-	                     });
-	                 parser.parseString(response, function (err, result)
-	                {
-	                  
-	                            
-	                 for(let answers of result.search_answers.search_answer) {
-	                                  	    console.log('answers d',answers);  
+                let parser = new xml2js.Parser(
+                       {
+                          trim: true,
+                          explicitArray: true
+                       });
+                   parser.parseString(response, function (err, result)
+                  {
+                    
+                              
+                   for(let answers of result.search_answers.search_answer) {
+                                          console.log('answers d',answers);  
 
-	                     	if (answers.items[0]=="			") {
+                         if (answers.items[0]=="      ") {
 
-	               		                //yyy.push({'title': 'Aucun resultat'}); 
-	              	             }else{
-	               
-	                   for(let item of answers.items){
-	                     
-	                     for(let i of item.item){
-	                       
-	                       //for(let i_data of i.item_data){
-	                          //console.log(i_data.data[1]._, i_data.data[1].$.name);
-	                          yyy.push({'title': i.name[0]});
-	                        /* for(let data of i_data.data){
-	                          
-	                           yyy.push({key: data.$.name, value: data._});
-	                           
-	                         }*/
-	                       //}
-	                     }
-	                   }
-	               }
-	                 }
-	                 resolve(yyy);
-	             
-	                });
-	            }, 
-	            error: function(error){
-	              console.log(error);
-	              reject();
+                                     //yyy.push({'title': 'Aucun resultat'}); 
+                               }else{
+                 
+                     for(let item of answers.items){
+                       
+                       for(let i of item.item){
+                         
+                         //for(let i_data of i.item_data){
+                            //console.log(i_data.data[1]._, i_data.data[1].$.name);
+                            yyy.push({'title': i.name[0]});
+                          /* for(let data of i_data.data){
+                            
+                             yyy.push({key: data.$.name, value: data._});
+                             
+                           }*/
+                         //}
+                       }
+                     }
+                 }
+                   }
+                   resolve(yyy);
+               
+                  });
+              }, 
+              error: function(error){
+                console.log(error);
+                reject();
 
-	            }
-	          });
-	        });
+              }
+            });
+          });
     }
     
 
--- src/pages/search-jaune/search-jaune.ts
+++ src/pages/search-jaune/search-jaune.ts
@@ -31,6 +31,7 @@ export class SearchJaunePage implements OnInit{
 
   currentLat;
   currentLng;
+  noResult: boolean = false; 
   private searchTerms = new Subject<string>();
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
@@ -100,18 +101,30 @@ export class SearchJaunePage implements OnInit{
    
         this.quiquoi =this.navParams.get('quiquoi');
         this.ou =this.navParams.get('ou');
+        this.currentLat =this.navParams.get('lat');
+        this.currentLng =this.navParams.get('lng');
+                  console.log('lat',this.currentLat);
+        console.log('lng',this.currentLng);
+        console.log('ou de recherche',this.ou);
          
-         this.geolocation.getCurrentPosition().then((resp)=>{
+        /* this.geolocation.getCurrentPosition().then((resp)=>{
         this.currentLat=resp.coords.latitude;
         this.currentLng=resp.coords.longitude;
         console.log('lat',this.currentLat);
         console.log('lng',this.currentLng);
-         });
+         });*/
   }
   ionViewDidEnter (){
-       if(this.quiquoi && this.ou){
-          this.list = this.onSubmitForm(this.quiquoi,this.ou);
-          console.log('quiquoi o:', this.quiquoi);
+       if(this.quiquoi && (this.ou=='Autour de moi' || this.ou=='autour de moi')){
+          this.list=this.go_build_quiquoi_approxy(this.quiquoi,this.currentLat,this.currentLng);
+
+          console.log('je ss in Autour de moi:', this.ou);
+                 console.log('lat',this.currentLat);
+        console.log('lng',this.currentLng);
+       }else{
+
+                   this.list = this.onSubmitForm(this.quiquoi,this.ou);
+          console.log('je ss in normalr:', this.ou);
        }    
   }
 
@@ -374,6 +387,111 @@ export class SearchJaunePage implements OnInit{
           });
 
     }
+       go_build_quiquoi_approxy(quiquoi,lat,lng){
+       this.go_search_quiquoi_approxy(quiquoi,lat,lng).then((data)=>{
+        this.list=data[0],this.count=data[1],
+        this.noResult =data[2]
+        console.log('datatattata',data);
+        console.log('datatattata',this.noResult);
 
+       })
+    }
+   go_search_quiquoi_approxy(quiquoi, lat, lng){
+         let list: any = [];
+         let count;
+         let i;
+         let noResult: boolean = false;
+          return new Promise((resolve,  reject) =>{
+            var start ='1';
+            var data_send  = '<?xml version="1.0" encoding="UTF-8" ?>';
+            data_send += '  <methodcall>';
+            data_send += '    <methodname call="approxy">'; 
+            data_send += '      <params>';  
+            data_send += '        <value>';
+            data_send += '          <string>'+quiquoi+'</string>';
+            data_send += '          <x>'+lat+'</x>';
+            data_send += '          <y>'+lng+'</y>';
+            data_send += '          <rayon>50000</rayon>';   
+            data_send += '          <start>'+start+'</start>';
+            data_send += '          <extract>10</extract>'; 
+            data_send += '        </value>';
+            data_send += '      </params>'; 
+            data_send += '    </methodname>';
+            data_send += '  </methodcall>';
+        
+            $.ajax({
+              
+              type       : "POST",
+              url        : "https://www.telecontact.ma/WsMobTlC2014nVZA",
+              /*headers: {accepts: '*'}:*/
+              crossDomain: true,
+              beforeSend : function() {$("#results_loading").append('<div class="noresults"><br /><br />Veuillez patienter<br /><br /><img src="media/images/home/load_result.gif" /></div>');/*$.mobile.loading('show')*/},
+              complete   : function() {$("#results_loading").hide();/*$.mobile.loading('hide')*/},
+              data       : {telecontact : data_send},
+              dataType   : 'text',
+              success    : function(response) {
+                                     
+                    response =response.replace(/&(?!(?:apos|quot|[gl]t|amp);|#)/g, '&amp;');
+                                    
+                let parser = new xml2js.Parser(
+                       {
+                          trim: true,
+                          explicitArray: true
+                       });
+
+                   parser.parseString(response, function (err, result)
+                  {
+                       console.log('result', result);
+                       if(result){
+                        count =result.search_answers.search_answer[0].items[0].$.count;
+                        
+                       //count=result.search_answers.search_answer.items[0].$.count;
+                   for(let answers of result.search_answers.search_answer) {                     
+                         if (answers.items[0]=="      ") {
+                                     list.push({'title': 'Aucun resultat'}); 
+                               }else{
+                     for(let item of answers.items){                             
+                      if(item.$.count>0)  {
+                       for(let i of item.item){            
+                         for(let i_data of i.item_data){    
+                              let d: any =[];
+                           for(let data of i_data.data){
+                              let   type=data.$.name;
+                            //console.log('type', type);                              
+                              d.push({[type]: data._});                         
+                             //list.push({key: data.$.name, value: data._});
+                             }
+                            list.push({d});                                                           
+                         }
+                       }
+                      }else{
+                        count =-1;
+                      } 
+                     }
+                 }
+                   }
+                   }else{
+                    list.push();
+                    noResult =true;
+                    console.log('Pas de résultats à proximité de vous.');
+                    console.log('Pas de résultats à proximité de vous.',noResult);
+
+                  }
+                   resolve([list,count,noResult]);
+                   console.log('dd :',list);
+                             //this.list=list;
+
+                  });
+              }, 
+              error: function(error){
+                console.log(error);
+                reject();
+
+              }
+            });
+          });
+
+    
+    }
 
 }
